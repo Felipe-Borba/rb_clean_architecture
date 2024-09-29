3e75c5098b3ed3dd0fd4d9e86214604d5b9cf083
Felipe Borba <felipe.borba0109@gmail.com>
2024-09-29 13:11:22 -0300

    add input to create a todo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-const todos = [
-  { description: 'Criar testes de unidade', done: false },
-  { description: 'Criar testes de integração', done: false },
-  { description: 'Criar testes de E2E', done: true }
-]
+import { reactive } from 'vue'
+
+const todos: any = reactive([])
+const description = ''
 
 function getTotal() {
   return todos.length
@@ -11,14 +10,20 @@ function getTotal() {
 
 function getCompleted() {
   const total = getTotal()
-  const completed = todos.filter((todo) => todo.done).length
+  const completed = todos.filter((todo: any) => todo.done).length
   return Math.round((completed / total) * 100)
+}
+
+function addTodo(description: string) {
+  todos.push({ description, done: false })
 }
 </script>
 
 <template>
   <div class="total">Total: {{ getTotal() }}</div>
   <div class="completed">Completed: {{ getCompleted() }}%</div>
+  <input type="text" class="todo-description-input" v-model="description" />
+  <button class="add-todo-button" @click="addTodo(description)">Add</button>
 </template>
 
 <style scoped></style>
